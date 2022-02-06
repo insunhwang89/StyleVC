c53b9a26d6321fd028b57a92b884ce9ddc4e62bf
INSUNHWANG <spsun89@gmail.com>
2022-02-06 15:22:49 +0900

    update inference and upload demo audio

--- vocoder/inference_npy.py
+++ vocoder/inference_npy.py
@@ -32,27 +32,28 @@ def scan_checkpoint(cp_dir, prefix):
         return ''
     return sorted(cp_list)[-1]
 
-def inference(a, option="npy"):
+# def inference(a, option="npy"):
+def inference(checkpoint_path, input_wavs_dir, output_dir, option="npy"):
 
     generator = Generator(h).to(device)
-    state_dict_g = load_checkpoint(a.checkpoint_path, device)
+    state_dict_g = load_checkpoint(checkpoint_path, device)
     generator.load_state_dict(state_dict_g['generator'])
 
     if option == "npy":
-        filelist = glob.glob(os.path.join(a.input_wavs_dir, '*.npy'))
+        filelist = glob.glob(os.path.join(input_wavs_dir, '*.npy'))
     else:
-        filelist = glob.glob(os.path.join(a.input_wavs_dir, '*.npz'))
-    if not os.path.exists(a.output_dir): 
-        os.makedirs(a.output_dir, exist_ok=True)
+        filelist = glob.glob(os.path.join(input_wavs_dir, '*.npz'))
+    if not os.path.exists(output_dir): 
+        os.makedirs(output_dir, exist_ok=True)
 
     generator.eval()
     generator.remove_weight_norm()
     with torch.no_grad():
-        for i, filename in enumerate(filelist):
+        for i, filname in enumerate(filelist):
 
             # Melspectrogram(from npy)
             if option == "npy":
-                x = np.load(filename)
+                x = np.load(filname)
                 x = torch.from_numpy(x).cuda()
                 if x.dim() == 2:
                     x = x.unsqueeze(0) # [B(1), 80, T]
@@ -66,13 +67,9 @@ def inference(a, option="npy"):
             audio = audio * MAX_WAV_VALUE
             audio = audio.cpu().numpy().astype('int16')
 
-            # scaling
-            # audio = normalize(audio) * 0.95
-            
             # Save Result
-            filename = filename.replace('\\', '/').split('/')[-1].split('.npy')[0] # ex: p226_016-to-p226_016
-            filename = filename.replace('wav', '')
-            output_file = os.path.join(a.output_dir, filename) + '.wav'
+            filname = filname.replace('\\', '/').split('/')[-1].split('.npy')[0] # ex: p226_016-to-p226_016
+            output_file = os.path.join(output_dir, filname + '.wav')
             write(output_file, h.sampling_rate, audio)
             print(output_file)
 
@@ -97,17 +94,14 @@ def main(hparams, path):
     checkpoint_path = os.path.join('vocoder/checkpoint/VCTK_16K_V1/g_01700000')
     config_path = os.path.join('vocoder/checkpoint/VCTK_16K_V1/config.json')
 
-    parser = argparse.ArgumentParser()
-    # parser.add_argument('--dataset', default=hparams.dataset)
-    parser.add_argument('--input_wavs_dir', default=os.path.join('generated', path))
-    parser.add_argument('--output_dir', default=os.path.join('generated', path))
-    parser.add_argument('--checkpoint_path', default=checkpoint_path)
-    parser.add_argument('--config_path', default=config_path)
-    a = parser.parse_args()
-
-    print("[*] Load checkpoint path: {}".format(a.checkpoint_path))
     
-    with open(a.config_path) as f:
+    input_wavs_dir = os.path.join('generated', path)
+    output_dir = os.path.join('generated', path)
+
+    print("[*] Load checkpoint path: {}".format(checkpoint_path))
+    
+
+    with open(config_path) as f:
         data = f.read()
 
     global h, device
@@ -121,8 +115,40 @@ def main(hparams, path):
     else:
         device = torch.device('cpu')
 
-    inference(a)
+    # inference(a)
+    inference(checkpoint_path, input_wavs_dir, output_dir)
 
 
 if __name__ == '__main__':
     main()
+
+
+#     parser = argparse.ArgumentParser()
+#     # parser.add_argument('--dataset', default=hparams.dataset)
+#     parser.add_argument('--input_wavs_dir', default=os.path.join('generated', path))
+#     parser.add_argument('--output_dir', default=os.path.join('generated', path))
+#     parser.add_argument('--checkpoint_path', default=checkpoint_path)
+#     parser.add_argument('--config_path', default=config_path)
+#     a = parser.parse_args()
+
+#     print("[*] Load checkpoint path: {}".format(a.checkpoint_path))
+    
+#     with open(a.config_path) as f:
+#         data = f.read()
+
+#     global h, device
+#     json_config = json.loads(data)
+#     h = AttrDict(json_config)
+
+#     torch.manual_seed(h.seed)
+#     if torch.cuda.is_available():
+#         torch.cuda.manual_seed(h.seed)
+#         device = torch.device('cuda')
+#     else:
+#         device = torch.device('cpu')
+
+#     inference(a)
+
+
+# if __name__ == '__main__':
+#     main()
